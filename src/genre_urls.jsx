ad53e82a29963142c97fb45dbf178c903d7e0cf5
code-af <afsalmanzil445@gmail.com>
2026-08-25 00:24:28 +0530

    added a url file that exports the api endpoints that can be reused along with the api key

---
+++ src/genre_urls.jsx
@@ -0,0 +1,7 @@
+import { API_KEY } from "./Constants/Constants"
+export const originals = `/discover/tv?api_key=${API_KEY}&with_networks=213`
+export const actionGenre = `/discover/movie?api_key=${API_KEY}&with_genres=28`
+export const comedyGenre = `/discover/movie?api_key=${API_KEY}&with_genres=35`
+export const horrorGenre = `/discover/movie?api_key=${API_KEY}&with_genres=27`
+export const romanceGenre = `/discover/movie?api_key=${API_KEY}&with_genres=10749`
+export const documentaries = `/discover/movie?api_key=${API_KEY}&with_genres=99`
