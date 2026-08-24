6115c2b619ef4d68d2221acfb761012a88072fdd
code-af <afsalmanzil445@gmail.com>
2026-08-25 02:56:41 +0530

    Imported and used the youtube npm package using onClick for a function that passes the id of the movie or show it will go to a function which checks if that id has any trailer videos using the video api from TMDB, if it's true then a key will be assigned to the state of urlKey and that will be used in Youtube Component at the end before the div using the options for video and also the urlKey as the videoId

--- src/Components/ContentCards/ContentCards.jsx
+++ src/Components/ContentCards/ContentCards.jsx
@@ -1,23 +1,40 @@
 import './ContentCards.css'
 import React,{useEffect, useState} from 'react'
-import {imageUrl} from '../../Constants/Constants'
+import {imageUrl, API_KEY} from '../../Constants/Constants'
 import axiosInstance from '../../axiosConfig'
+import YouTube from 'react-youtube'
 
 function ContentCards(props){
+    const [urlKey, setUrlKey] = useState('')
     const [movies, setMovies] = useState([])
     useEffect(()=>{
         axiosInstance.get(props.url).then((response)=>{
             setMovies(response.data.results)
         }).catch(err=>alert("Something went wrong ☹️"))
     },[])
+    const options = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+    const handleVideos = (id)=>{
+        axiosInstance.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+            const video = response.data.results.find(videoInfo => videoInfo.name.toLowerCase().includes("trailer"))
+            video ? setUrlKey(video.key) : console.log('No Videos Available')
+        })
+    }
     return(
         <div className='content-row'>
             <h2>{props.title}</h2>
             <div className="posters">
                 {movies.map((movie)=>
-                    <img className='poster-image' src={props.isOriginal ? `${imageUrl+movie.backdrop_path}` : `${imageUrl+movie.poster_path}`} alt="Poster" />
+                    <img onClick={()=>{handleVideos(movie.id)}} className='poster-image' src={props.isOriginal ? `${imageUrl+movie.backdrop_path}` : `${imageUrl+movie.poster_path}`} alt="Poster" />
                 )}
             </div>
+            {urlKey &&  <YouTube opts={options} videoId={urlKey}/>}
         </div>
     )
 }
