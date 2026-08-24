4491b15a82c3caa7fce6b1fc2c0a4fc3199dd142
code-af <afsalmanzil445@gmail.com>
2026-08-24 21:32:53 +0530

    Inside banner component imported API key, axios Instance, useState and useEffect hooks. For setting a movie used useState also used useEffect for operations at mounting it will get data from the api endpoint, then used Math library for selecting a random movie array number to select the movie. Also for title and description used ternary operator for displaying movie details

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,16 +1,27 @@
+import React, {useEffect, useState} from 'react'
 import './Banner.css'
-import React from 'react'
+import {API_KEY} from '../../Constants/Constants'
+import axiosInstance from '../../axiosConfig' 
+
 function Banner(){
+    const [movie, setMovie] = useState()
+    useEffect(()=>{
+        axiosInstance.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            const randomMedia = Math.floor(Math.random() * response.data.results.length)
+            console.log(randomMedia)
+            setMovie(response.data.results[randomMedia])
+            console.log(response.data.results[randomMedia])
+        })
+    },[])
     return(
         <div className='banner'>
             <div className='content'>
-                <h1 className='title-name'>Movie Name</h1>
+                <h1 className='title-name'>{movie ? movie.name || movie.title : ""}</h1>
                 <div className='banner-btns'>
                     <button className='btn'>Play</button>
                     <button className='btn'>My List</button>
                 </div>
-                <h1 className='description'>It is a long established fact that a reader will be distracted
-                     by the readable content of a page when looking at its layout. </h1>
+                <h1 className='description'>{movie ? movie.overview : ""}</h1>
             </div>
         <div className="bottom-fade">
             
