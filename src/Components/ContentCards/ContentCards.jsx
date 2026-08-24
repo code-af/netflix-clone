1d1ac4472d594bb344a89453f04ab759fb3017e3
code-af <afsalmanzil445@gmail.com>
2026-08-25 00:28:07 +0530

    Since there is more rows of genre to be added, the axios url argument has been replaced with the props url that is passed from app.jsx file, also made some changes to the title dynamically based on props.title from apps along with props.isOriginal to check the condition if it's netflix original genre then show a big picture otherwise just poster cards for the genre's

--- src/Components/ContentCards/ContentCards.jsx
+++ src/Components/ContentCards/ContentCards.jsx
@@ -1,22 +1,21 @@
 import './ContentCards.css'
 import React,{useEffect, useState} from 'react'
-import {API_KEY, imageUrl} from '../../Constants/Constants'
+import {imageUrl} from '../../Constants/Constants'
 import axiosInstance from '../../axiosConfig'
 
-function ContentCards(){
+function ContentCards(props){
     const [movies, setMovies] = useState([])
     useEffect(()=>{
-        axiosInstance.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response)=>{
-            console.log(response.data.results)
+        axiosInstance.get(props.url).then((response)=>{
             setMovies(response.data.results)
         }).catch(err=>alert("Something went wrong ☹️"))
     },[])
     return(
         <div className='content-row'>
-            <h2>Netflix Originals</h2>
+            <h2>{props.title}</h2>
             <div className="posters">
                 {movies.map((movie)=>
-                    <img className='poster-image' src={`${imageUrl+movie.poster_path}`} alt="Poster" />
+                    <img className='poster-image' src={props.isOriginal ? `${imageUrl+movie.backdrop_path}` : `${imageUrl+movie.poster_path}`} alt="Poster" />
                 )}
             </div>
         </div>
