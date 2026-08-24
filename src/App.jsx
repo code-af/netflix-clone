f2e9f00b081f71885d1592a7b90f825b9c081691
code-af <afsalmanzil445@gmail.com>
2026-08-24 20:12:08 +0530

    Added an asset md for setting up the links for the api endpoints, also added a new directory and file for constant variable that can be reused on any component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <NavBar/>
       <Banner/>
-      <ContentCards/>
+      <ContentCards/>   
     </>
   )
 }
---
+++ src/Constants/Constants.jsx
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = import.meta.env.VITE_TMDB_KEY
