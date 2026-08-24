216aa5edbf0306b644da679c109f18bc262d5e0e
code-af <afsalmanzil445@gmail.com>
2026-08-25 00:25:43 +0530

    Reused the content cards for multiple rows of content for different genre while passing props dynamically each time for title and the url of api endpoint of that genre

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import NavBar from './Components/NavBar/NavBar.jsx'
 import Banner from './Components/Banner/Banner.jsx'
 import ContentCards from './Components/ContentCards/ContentCards.jsx'
+import {originals, actionGenre, comedyGenre, horrorGenre, romanceGenre, documentaries} from './genre_urls.jsx'
 
 function App() {
   return(
     <>
       <NavBar/>
       <Banner/>
-      <ContentCards/>   
+      <ContentCards url={originals} title = "Netflix Originals" isOriginal />
+      <ContentCards url={actionGenre} title = "Action"/>   
+      <ContentCards url={comedyGenre} title = "Comedy"/>   
+      <ContentCards url={horrorGenre} title = "Horror"/>   
+      <ContentCards url={romanceGenre} title = "Romance"/>   
+      <ContentCards url={documentaries} title = "Documentaries"/>   
     </>
   )
 }
